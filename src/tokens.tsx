84b3f0364b654062deafc3ee5f478a7e8c44d536
samj663 <samuelj246@icloud.com>
2023-11-01 20:39:51 -0400

    Changes api url conditions in tokens.tsx

--- src/tokens.tsx
+++ src/tokens.tsx
@@ -7,7 +7,8 @@
 export var REACT_APP_MAPBOX_STYLE_MONOCHROME = process.env.REACT_APP_MAPBOX_STYLE_MONOCHROME || "mapbox://styles/samuelj246/cllmeecax02ik01p89omef3g2"
 export var REACT_APP_MAPBOX_KEY = process.env.REACT_APP_MAPBOX_KEY_PUBLIC || "pk.eyJ1Ijoic2FtdWVsajI0NiIsImEiOiJjbGxrMHgzOWwwdjkwM2VwYXpnMGdieHluIn0.LTf2Lj9kuERyDhhWYGHkOw"
 export var REACT_APP_MAPBOX_STYLE = process.env.REACT_APP_MAPBOX_STYLE || 'mapbox://styles/samuelj246/clj0ed7g7018v01qi7k96f2oe'
-export const API_URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? process.env.REACT_APP_API_URL : 'https://wmata-backend.onrender.com'
+console.log(process.env.REACT_APP_API_URL)
+export const API_URL = ((process.env.REACT_APP_API_URL == undefined)) ?  'https://wmata-backend.onrender.com':process.env.REACT_APP_API_URL;
 /*
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     API_URL = process.env.REACT_APP_API_URL
