a0bdc8528dfc5ad999f6c2d58902a8f922f48448
samj663 <samuelj246@icloud.com>
2024-05-23 21:30:44 -0400

    Added timezone to database.service_id_today()

--- api/database.tsx
+++ api/database.tsx
@@ -58,7 +58,7 @@ export async function get_next_bus(stop_id: string){
 
 async function service_id_today(){
   let sql = postgres(process.env.local_url, { ssl: true });
-  let date = new Date().toLocaleDateString("af-ZA").replace(/-/g,"")
+  let date = new Date().toLocaleDateString("af-ZA",{timeZone: 'America/New_York'}).replace(/-/g,"")
   let service_exception = await sql`select service_id from bus_calendar_dates where service_date = ${date} and exception_type = 1 limit 1`
   var output;
   if(service_exception.length > 0){
