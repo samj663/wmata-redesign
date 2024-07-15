af0a4005b64f0d0f791c957827eccc119461bcca
samj663 <samuelj246@icloud.com>
2024-07-15 19:26:32 -0400

    Fixing schedule issue

--- api/database.tsx
+++ api/database.tsx
@@ -421,6 +421,7 @@ export async function update_rail_data() {
       let sql = postgres(database_url);
       try{
           let date = new Date()
+          console.log(date.toLocaleDateString("af-ZA",{timeZone: 'America/New_York'}))
           let service_exception = await sql`select service_id from rail_calendar_dates where service_date = ${date.toLocaleDateString("af-ZA",{timeZone: 'America/New_York'}).replace(/-/g,"")} and exception_type = 1`
           let todays_service = service_exception.map((a:any) => a.service_id);
           let start_time = new Date()
@@ -430,7 +431,7 @@ export async function update_rail_data() {
           let temp = start_time.toLocaleTimeString('it-IT',{timeZone: 'America/New_York'}).toString()
           let temp2 = end_time.toLocaleTimeString('it-IT',{timeZone: 'America/New_York'}).toString()
           let output;
-          console.log(`${start_time} -- ${end_time}`)
+          console.log(`${temp} -- ${temp2}`)
           console.log(todays_service)
        //   console.log()
           if(parseInt(temp.slice(0,2)) == 23 && parseInt(temp2.slice(0,2)) < 2){
