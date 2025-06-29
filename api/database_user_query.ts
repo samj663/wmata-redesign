99271504eeb72577e1d3adb1bbf5d91c450a38f7
samj663 <samuelj246@icloud.com>
2025-06-29 12:13:45 -0400

    Fixed bug with get_bus_route_stops()

--- api/database_user_query.ts
+++ api/database_user_query.ts
@@ -359,7 +359,7 @@ export async function get_bus_route_stops(route: String){
   let sql = postgres(database_user_pool_url);
   try {
     let output:any = await sql`
-    SELECT DISTINCT bus_stops.stop_id, bus_stops.stop_name, direction_id, stop_sequence, trip_headsign, shape_id
+    SELECT DISTINCT bus_stops.stop_id, bus_stops.stop_code, bus_stops.stop_name, direction_id, stop_sequence, trip_headsign, shape_id
     FROM bus_trips
     INNER JOIN bus_stop_times ON bus_stop_times.trip_id = bus_trips.trip_id
     INNER JOIN bus_stops ON bus_stops.stop_id = bus_stop_times.stop_id
